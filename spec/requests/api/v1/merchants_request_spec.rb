baf71aa4d8766df882e964be573f6e8d4dcedb05
Charles Ren <cren04@syr.edu>
2024-02-05 14:25:51 -0800

    test & factory creation: merchants index test, customers & merchants factory creation

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -3,8 +3,21 @@ require 'rails_helper'
 RSpec.describe 'Merchants API' do
   # US 1
   describe 'Merchants Index' do
-    it 'returns all m' do
+    it 'returns all merchants' do
+      create(:merchant, 3)
 
+      get '/api/v1/merchants'
+
+      expect(response).to be_successful
+
+      merchants = JSON.parse(response.body, symbolize_names: true)
+
+      expect(merchants.count).to eq(3)
+
+      merchants.each do |merchant|
+        expect(merchant).to have_key(:name)
+        expect(merchant[:name]).to be_an(String)
+      end
     end
   end
 end
