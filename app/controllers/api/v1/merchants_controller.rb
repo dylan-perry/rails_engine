d7b06006a576fcae74d4f333393c369d7cdb79dc
Charles Ren <cren04@syr.edu>
2024-02-06 10:34:50 -0800

    feat: serializer

---
+++ app/controllers/api/v1/merchants_controller.rb
@@ -0,0 +1,6 @@
+class Api::V1::MerchantsController < ApplicationController
+  def index
+    merchants = Merchant.all
+    render json: MerchantSerializer.new(merchants)
+  end
+end
--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -4,19 +4,19 @@ RSpec.describe 'Merchants API' do
   # US 1
   describe 'Merchants Index' do
     it 'returns all merchants' do
-      create(:merchant, 3)
+      create_list(:merchant, 3)
 
       get '/api/v1/merchants'
 
       expect(response).to be_successful
 
       merchants = JSON.parse(response.body, symbolize_names: true)
+# require 'pry';binding.pry
+      expect(merchants[:data].count).to eq(3)
 
-      expect(merchants.count).to eq(3)
-
-      merchants.each do |merchant|
-        expect(merchant).to have_key(:name)
-        expect(merchant[:name]).to be_an(String)
+      merchants[:data].each do |merchant|
+        expect(merchant[:attributes]).to have_key(:name)
+        expect(merchant[:attributes][:name]).to be_an(String)
       end
     end
   end
