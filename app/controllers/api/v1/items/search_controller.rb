7a93cc72391945bd73c09ea761b52f480332e8f2
Dylan Perry <dylanbperry@gmail.com>
2024-02-08 14:33:19 -0800

    Pass happy path testing for Find One Item

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::Items::SearchController < ApplicationController
     def find
-        require 'pry'; binding.pry
-        render json: ItemSerializer.new(Item.find_by(params[:name]).order(name: :desc).first)
+        # require 'pry'; binding.pry
+        render json: ItemSerializer.new(Item.where("name ILIKE ?", "%#{params[:name]}%").order(name: :asc).first)
+        # Item.where("name LIKE ?", "%Choco%")c
     end
 end
